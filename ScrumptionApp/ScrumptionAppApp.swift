8a0400075974c7ff57cf50e5017bb5a638daabe9
Mohamed Ayed <engmohammedayed@gmail.com>
2021-02-25 21:38:06 +0200

    Initial Commit

---
+++ ScrumptionApp/ScrumptionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumptionAppApp.swift
+//  ScrumptionApp
+//
+//  Created by mohamed ayed on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumptionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
